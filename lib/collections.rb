3fb872c50d44ecbf819e40c7bb7a160903dffacc
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2017-02-28 12:27:19 -0500

    modified collections to handle external manifeset in author collections as well as expression collections

--- lib/collections.rb
+++ lib/collections.rb
@@ -56,14 +56,20 @@ end
 def create_person_collection(personid)
 
   query = "
-  SELECT ?m ?m_shortid ?m_label
+  SELECT ?m ?m_shortid ?m_label (COUNT(?surface) AS ?count)
   {
     ?expression <http://www.loc.gov/loc.terms/relators/AUT> <http://scta.info/resource/#{personid}> .
     ?expression <http://scta.info/property/level> '1' .
     ?expression <http://scta.info/property/hasManifestation> ?m .
     ?m <http://purl.org/dc/elements/1.1/title> ?m_label .
     ?m <http://scta.info/property/shortId> ?m_shortid .
+    OPTIONAL
+    {
+      ?m <http://scta.info/property/hasStructureItem> ?item .
+      ?item <http://scta.info/property/hasSurface> ?surface .
+    }
   }
+  GROUP BY ?m_shortid ?m ?m_label
   "
   #@results = rdf_query(query)
   query_obj = Lbp::Query.new()
@@ -76,11 +82,23 @@ def create_person_collection(personid)
   results.each do |result|
     #temporary conditional to weed out, born-digital manifestations; should be using a manifestationType property
     unless result[:m_shortid].to_s.include? "critical"
-      manifest = {
+      if result[:count] < 1
+        # TODO this string parsing - split() is bad!!!, value should be retrievable from query
+        manifestid = get_official_manifest_url(result[:m_shortid].to_s.split("/").last)
+        manifestid = if manifestid == "http://localhost:3030/ds/data?default" then "#{idbase}/iiif/#{result[:m_shortid]}/manifest" else manifestid end
+        manifest = {
+          "@id": manifestid,
+          "@type": "sc:Manifest",
+          "label": result[:m_label]
+        }
+      else
+        manifest = {
         "@id": "#{idbase}/iiif/#{result[:m_shortid]}/manifest",
         "@type": "sc:Manifest",
         "label": result[:m_label]
       }
+      end
+
     manifests << manifest
     end
 
