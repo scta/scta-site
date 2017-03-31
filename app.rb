146c251be4a02fadf5b818d175acaa71a9997afb
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2017-03-31 11:52:57 -0400

    fixed error in title count

--- app.rb
+++ app.rb
@@ -148,13 +148,13 @@ get '/' do
     # @itemcount = rdf_query.query(itemquery).first[:".1"]
     # @commentarycount = rdf_query.query(commentaryquery).first[:".1"]
     # @namecount = rdf_query.query(namequery).first[:".1"]
-    # @workcount = rdf_query.query(workquery).first[:".1"]
+    #@workcount = rdf_query.query(workquery).first[:".1"]
     # @totalcount = rdf_query.query(totalquery).first[:count].to_i
     @quotationcount = "13,254"
     @itemcount = "13,960"
     @commentarycount = "74"
     @namecount = "9,132"
-    @workcount = "9,132"
+    @workcount = "2,570"
     @totalcount = "2,487,202"
 
 
