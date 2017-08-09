ad4466446bb200cc3f33eab38ea0302051e9a27a
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-08-09 14:25:24 -0400

    modified some xml errors in oai responses

--- lib/oai_functions.rb
+++ lib/oai_functions.rb
@@ -147,7 +147,7 @@ def list_sets
       <setName>Sentences Commentaries</setName>
       <setDescription>
       <oai_dc:dc
-          xmlns:oai_dc='http://www.openarchives.org/OAI/2.0/oai_dc/''
+          xmlns:oai_dc='http://www.openarchives.org/OAI/2.0/oai_dc/'
           xmlns:dc='http://purl.org/dc/elements/1.1/'
           xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
           xsi:schemaLocation='http://www.openarchives.org/OAI/2.0/oai_dc/
