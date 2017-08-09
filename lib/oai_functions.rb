3a40d54f27cdef3150e4c85024ac4bffcf48c501
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-08-09 14:43:28 -0400

    added sparql query to retrieve top level expressions

--- lib/oai_functions.rb
+++ lib/oai_functions.rb
@@ -70,34 +70,21 @@ def identify
   return content
 end
 def list_identifiers
+  records = getExpressions("scta")
+  combined_records = ""
+  records.each do |record|
+    record = "<record>
+      <header>
+        <identifier>#{record[:expression]}</identifier>
+        <datestamp>2001-12-14</datestamp>
+        <setSpec>scta</setSpec>
+      </header>
+    </record>"
+    combined_records = combined_records + record
+  end
+
   content = "<ListIdentifiers>
-    <header>
-      <identifier>http://scta.info/resource/plaoulcommentary</identifier>
-      <datestamp>1999-03-20</datestamp>
-      <setSpec>sententia</setSpec>
-    </header>
-    <!--
-    <header>
-      <identifier>oai:arXiv.org:hep-th/9801002</identifier>
-      <datestamp>1999-03-20</datestamp>
-      <setSpec>physic:hep</setSpec>
-      <setSpec>physic:exp</setSpec>
-    </header>
-    <header>
-      <identifier>oai:arXiv.org:hep-th/9801005</identifier>
-      <datestamp>2000-01-18</datestamp>
-      <setSpec>physic:hep</setSpec>
-    </header>
-    <header status='deleted'>
-      <identifier>oai:arXiv.org:hep-th/9801010</identifier>
-      <datestamp>1999-02-23</datestamp>
-      <setSpec>physic:hep</setSpec>
-      <setSpec>math</setSpec>
-    </header>
-    <resumptionToken expirationDate='2002-06-01T23:20:00Z'
-      completeListSize='6'
-      cursor='0'>xxx45abttyz</resumptionToken>
-      -->
+    #{combined_records}
   </ListIdentifiers>"
   return content
 end
@@ -113,13 +100,15 @@ def list_metadata_formats
   </ListMetadataFormats>"
   return content
 end
-def list_records
-  content = "<ListRecords>
-    <record>
+def list_records()
+  records = getExpressions("scta")
+  combined_records = ""
+  records.each do |record|
+    record = "<record>
       <header>
-        <identifier>http://scta.info/resource/plaoulcommentary</identifier>
+        <identifier>#{record[:expression]}</identifier>
         <datestamp>2001-12-14</datestamp>
-        <setSpec>sententia</setSpec>
+        <setSpec>scta</setSpec>
       </header>
       <metadata>
         <oai_dc:dc
@@ -128,22 +117,38 @@ def list_records
           xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
           xsi:schemaLocation='http://www.openarchives.org/OAI/2.0/oai_dc/
           http://www.openarchives.org/OAI/2.0/oai_dc.xsd'>
-          <dc:title>Plaoul Commentary</dc:title>
-          <dc:creator>Peter Plaoul</dc:creator>
-          <dc:subject>Digital Libraries</dc:subject>
-          <dc:description>Dublin core description
-          </dc:description>
+          <dc:title>#{record[:expression_label]}</dc:title>
+          <dc:creator>#{record[:author_title]}</dc:creator>
+          <dc:description>#{record[:expression_description]}</dc:description>
           <dc:date>2001-12-14</dc:date>
         </oai_dc:dc>
       </metadata>
-    </record>
-    <!-- more records can be added here -->
+    </record>"
+    combined_records = combined_records + record
+  end
+
+  content = "<ListRecords>
+    #{combined_records}
   </ListRecords>"
 end
 def list_sets
   content = "<ListSets>
+  <set>
+    <setSpec>SCTA</setSpec>
+    <setName>Scholastic Commentaries and Texts Archive</setName>
+    <setDescription>
+    <oai_dc:dc
+        xmlns:oai_dc='http://www.openarchives.org/OAI/2.0/oai_dc/'
+        xmlns:dc='http://purl.org/dc/elements/1.1/'
+        xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+        xsi:schemaLocation='http://www.openarchives.org/OAI/2.0/oai_dc/
+        http://www.openarchives.org/OAI/2.0/oai_dc.xsd'>
+        <dc:description>Top Level Collection for the Scholastic Commentaries and Texts Archive</dc:description>
+     </oai_dc:dc>
+  </setDescription>
+  </set>
     <set>
-      <setSpec>sententia</setSpec>
+      <setSpec>SCTA: (sententia)</setSpec>
       <setName>Sentences Commentaries</setName>
       <setDescription>
       <oai_dc:dc
@@ -157,5 +162,25 @@ def list_sets
     </setDescription>
     </set>
   </ListSets>"
+end
+# this funciton basically repeats part of create_wg_collection(wg_shortid) in collections.rb;
+# TODO: this should be refactored
+def getExpressions(wg_shortid)
+  query = "
+  SELECT ?expression ?expression_shortid ?expression_label ?author_title ?expression_description ?wg_label
+  {
+    <http://scta.info/resource/#{wg_shortid}> <http://scta.info/property/hasExpression> ?expression .
+    <http://scta.info/resource/#{wg_shortid}> <http://purl.org/dc/elements/1.1/title> ?wg_label .
+    ?expression <http://purl.org/dc/elements/1.1/title> ?expression_label .
+    ?expression <http://www.loc.gov/loc.terms/relators/AUT> ?expression_author .
+    ?expression_author <http://purl.org/dc/elements/1.1/title> ?author_title .
+    ?expression <http://purl.org/dc/elements/1.1/description> ?expression_description .
+    ?expression <http://scta.info/property/shortId> ?expression_shortid .
+  }
+  "
 
+  #@results = rdf_query(query)
+  query_obj = Lbp::Query.new()
+  results = query_obj.query(query)
+  return results
 end
